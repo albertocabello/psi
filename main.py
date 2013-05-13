5fd713a03206de143f52b1ac910fbb74291406fb
albertocabello <alberto@unex.es>
2013-05-13 18:26:58 +0200

    Just discovered wx.ColourPickerCtrl ;)

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class PSI(wx.App):
                         id = xrc.XRCID(u'Quit'))
         self.frame.Bind(wx.EVT_MENU, self.OpenFile,
                         id = xrc.XRCID(u'Open'))
-        self.frame.Bind(wx.EVT_MENU, self.StyleDialog,
+        self.frame.Bind(wx.EVT_MENU, self.ShowStyleDialog,
                                 id = xrc.XRCID(u'DrawingStyle'))
         self.frame.Bind(wx.EVT_MENU, self.LR,
                                 id = xrc.XRCID(u'LR'))
@@ -56,7 +56,7 @@ class PSI(wx.App):
         for i in self.mainGrid.GetSelectedCellsList('N'):
             print i
 
-    def StyleDialog(self, e):
+    def ShowStyleDialog(self, e):
         styleDialog = self.res.LoadDialog(self.frame, u'StyleDialog')
         radius = self.props.GetProperty('style', 'dots-radius')
         xrc.XRCCTRL(styleDialog, u'DotsRadius').SetValue(int(radius))
@@ -64,49 +64,33 @@ class PSI(wx.App):
         self.dots_g = self.props.GetProperty('style', 'dots-color-g')
         self.dots_b = self.props.GetProperty('style', 'dots-color-b')
         color = wx.Color(int(self.dots_r), int(self.dots_g), int(self.dots_b))
-        DotsColorButton = xrc.XRCCTRL(styleDialog, u'DotsColor')
-        DotsColorButton.SetForegroundColour(color)
-        styleDialog.Bind(wx.EVT_BUTTON, self.SetColors,
-            id = xrc.XRCID(u'DotsColor'))
+        DotsColorCtrl = xrc.XRCCTRL(styleDialog, u'DotsColor')
+        DotsColorCtrl.SetColour(color)
         width = self.props.GetProperty('style', 'line-width')
         xrc.XRCCTRL(styleDialog, u'LineWidth').SetValue(int(width))
+        LineColorCtrl = xrc.XRCCTRL(styleDialog, u'LineColor')
         self.line_r = self.props.GetProperty('style', 'line-color-r')
         self.line_g = self.props.GetProperty('style', 'line-color-g')
         self.line_b = self.props.GetProperty('style', 'line-color-b')
         color = wx.Color(int(self.line_r), int(self.line_g), int(self.line_b))
-        LineColorButton = xrc.XRCCTRL(styleDialog, u'LineColor')
-        LineColorButton.SetForegroundColour(color)
-        styleDialog.Bind(wx.EVT_BUTTON, self.SetColors,
-            id = xrc.XRCID(u'LineColor'))
+        LineColorCtrl.SetColour(color)
         if styleDialog.ShowModal() == wx.ID_OK:
+            self.dots_r = DotsColorCtrl.GetColour().Red()
+            self.dots_g = DotsColorCtrl.GetColour().Green()
+            self.dots_b = DotsColorCtrl.GetColour().Blue()            
             self.props.SetProperty('style', 'dots-color-r', self.dots_r)
             self.props.SetProperty('style', 'dots-color-g', self.dots_g)
             self.props.SetProperty('style', 'dots-color-b', self.dots_b)
             radius = xrc.XRCCTRL(styleDialog, u'DotsRadius').GetValue()
+            self.line_r = LineColorCtrl.GetColour().Red()
+            self.line_g = LineColorCtrl.GetColour().Green()
+            self.line_b = LineColorCtrl.GetColour().Blue()            
             self.props.SetProperty('style', 'dots-radius', radius)
             self.props.SetProperty('style', 'line-color-r', self.line_r)
             self.props.SetProperty('style', 'line-color-g', self.line_g)
             self.props.SetProperty('style', 'line-color-b', self.line_b)
             width = xrc.XRCCTRL(styleDialog, u'LineWidth').GetValue()
             self.props.SetProperty('style', 'line-width', width)
-
-    def SetColors(self, e):
-        colorDialog = wx.ColourDialog(None)
-        styleDialog = self.res.LoadDialog(self.frame, u'StyleDialog')
-        if colorDialog.ShowModal() == wx.ID_OK:
-            color = colorDialog.GetColourData().GetColour()
-            button = wx.Button(None)
-            if e.GetId() == xrc.XRCID(u'DotsColor'):
-                self.dots_r = color.Red()
-                self.dots_g = color.Green()
-                self.dots_b = color.Blue()
-                button = xrc.XRCCTRL(e.GetEventObject(), u'DotsColor')
-            elif e.GetId() == xrc.XRCID(u'LineColor'):
-                self.line_r = color.Red()
-                self.line_g = color.Green()
-                self.line_b = color.Blue()
-                button = xrc.XRCCTRL(e.GetEventObject(), u'LineColor')
-            button.SetForegroundColour(color)
 
     def Close(self, e):
         print 'Closing application...'
